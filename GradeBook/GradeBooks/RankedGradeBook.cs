25622b0b5e2fe89b230d5b2231f2518babc38808
OsKaRaStYYY <o.kupczynski@gmail.com>
2023-03-11 11:32:40 +0100

    Update RankedGradeBook.cs
    complete override rankedgradebook's getlettergrade metod

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,14 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name ) : base( name )
+        public RankedGradeBook(string name) : base(name)
         {
 
         }
-    }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int StudentProcent = (int)Math.Ceiling(Students.Count * 0.2);
+            List<double> Grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+            int index = Grades.IndexOf(averageGrade);
+            if (index < StudentProcent)
+            {
+                return 'A';
+            }
+            else if (index < StudentProcent * 2)
+            {
+                return 'B';
+
+            }
+            else if (index < StudentProcent * 3)
+            {
+                return 'C';
+
+            }
+            else if (index < StudentProcent * 4)
+            {
+
+                return 'D';
+            }
+            else 
+            {
+                return 'F';
+            }
+        }
+    
+    }  
+    
 }
