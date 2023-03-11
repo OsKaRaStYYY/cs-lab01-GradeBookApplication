3d7710e28e680ce250f787b36ecfdc47c547a905
OsKaRaStYYY <o.kupczynski@gmail.com>
2023-03-11 11:39:09 +0100

    Update RankedGradeBook.cs
    CalculateStatistics method and CalculateStudentStatistics method completed

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,6 +15,18 @@ namespace GradeBook.GradeBooks
 
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+
+            base.CalculateStatistics();
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
 
@@ -50,7 +62,18 @@ namespace GradeBook.GradeBooks
                 return 'F';
             }
         }
-    
+
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }  
     
 }
