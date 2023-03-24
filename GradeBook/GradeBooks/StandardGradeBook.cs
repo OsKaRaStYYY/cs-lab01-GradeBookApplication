e2d1ed010884f5b8cd34c17a082e49bade74da68
OsKaRaStYYY <o.kupczynski@gmail.com>
2023-03-24 20:48:02 +0100

    Update StandardGradeBook.cs
    standardgradebook

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -8,7 +8,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
-
+            Type = Enums.GradeBookType.Standard;
         }
     }
 }
